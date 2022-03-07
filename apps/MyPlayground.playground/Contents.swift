7c6c65306c09a7781683627677a81b1b4bf96b5f
rqalqallaf <rq.alqallaf@gmail.com>
2022-03-07 20:33:52 +0300

    done

---
+++ apps/MyPlayground.playground/Contents.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+var greeting = "Hello, playground"
+let name = "abdulrazaq alqallaf"
+let favourtieNumber = 3
+var MyHobby = "Debate,runnig"
+print ("السلام عليكم، معاكم \(name) ورقمي المفضل \(favourtieNumber) واحب \(MyHobby) ")
+let numberone = 1
+let numbertwo = 2.5
---
+++ apps/apps/appsApp.swift
@@ -0,0 +1,17 @@
+//
+//  appsApp.swift
+//  apps
+//
+//  Created by GK on 05/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct appsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
